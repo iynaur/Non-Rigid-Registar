01aaf186ee032bac8ed944c3972e330fe860e105
Bing Jian <bing.jian@jd.com>
2019-05-10 11:35:09 -0700

    Code formating using clang-format.

--- src/optimal_nonrigid_icp/main.cpp
+++ src/optimal_nonrigid_icp/main.cpp
@@ -1,99 +1,109 @@
+#include <vtkSmartPointer.h>
 #include <iostream>
 #include <string>
-#include <vtkSmartPointer.h>
 //#include <vtkOBJReader.h>
+#include <vtkActor.h>
+#include <vtkInteractorStyleRubberBandPick.h>
 #include <vtkPLYReader.h>
+#include <vtkPLYWriter.h>
 #include <vtkPolyData.h>
 #include <vtkPolyDataMapper.h>
-#include <vtkActor.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkInteractorStyleRubberBandPick.h>
 #include <vtkProperty.h>
-#include <vtkPLYWriter.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
 
 #include <stdio.h>
 
 #include "optimal_nonrigid_icp.h"
 
-int main(int argc, char**argv)
-{
-	std::string template_filename = argv[1];
-	std::string target_filename = argv[2];
+int main(int argc, char** argv) {
+  std::string template_filename = argv[1];
+  std::string target_filename = argv[2];
 
-	// vtkSmartPointer<vtkOBJReader> template_reader = vtkSmartPointer<vtkOBJReader>::New();
-	// template_reader->SetFileName(template_filename.c_str());
-	// template_reader->Update();
+  // vtkSmartPointer<vtkOBJReader> template_reader =
+  // vtkSmartPointer<vtkOBJReader>::New();
+  // template_reader->SetFileName(template_filename.c_str());
+  // template_reader->Update();
 
-	// vtkSmartPointer<vtkOBJReader> target_reader = vtkSmartPointer<vtkOBJReader>::New();
-	// target_reader->SetFileName(target_filename.c_str());
-	// target_reader->Update();
+  // vtkSmartPointer<vtkOBJReader> target_reader =
+  // vtkSmartPointer<vtkOBJReader>::New();
+  // target_reader->SetFileName(target_filename.c_str());
+  // target_reader->Update();
 
-	vtkSmartPointer<vtkPLYReader> template_reader = vtkSmartPointer<vtkPLYReader>::New();
-	template_reader->SetFileName(template_filename.c_str());
-	template_reader->Update();
+  vtkSmartPointer<vtkPLYReader> template_reader =
+      vtkSmartPointer<vtkPLYReader>::New();
+  template_reader->SetFileName(template_filename.c_str());
+  template_reader->Update();
 
-	vtkSmartPointer<vtkPLYReader> target_reader = vtkSmartPointer<vtkPLYReader>::New();
-	target_reader->SetFileName(target_filename.c_str());
-	target_reader->Update();
+  vtkSmartPointer<vtkPLYReader> target_reader =
+      vtkSmartPointer<vtkPLYReader>::New();
+  target_reader->SetFileName(target_filename.c_str());
+  target_reader->Update();
 
-  	vtkSmartPointer<vtkPolyData> template_polyData = template_reader->GetOutput();
-	vtkSmartPointer<vtkPolyData> target_polyData = target_reader->GetOutput();
+  vtkSmartPointer<vtkPolyData> template_polyData = template_reader->GetOutput();
+  vtkSmartPointer<vtkPolyData> target_polyData = target_reader->GetOutput();
 
-	OptimalNonrigidICP oni(template_polyData, target_polyData);
-	oni.init();
-	std::cout << "init() finished!" << std::endl;
+  OptimalNonrigidICP oni(template_polyData, target_polyData);
+  oni.init();
+  std::cout << "init() finished!" << std::endl;
 
+  vtkSmartPointer<vtkPLYWriter> plyWriter =
+      vtkSmartPointer<vtkPLYWriter>::New();
+  plyWriter->SetInputData(template_polyData);
 
-	vtkSmartPointer<vtkPLYWriter> plyWriter = vtkSmartPointer<vtkPLYWriter>::New();
-	plyWriter->SetInputData(template_polyData);
+  float alpha = 10.0;
+  float gamma = 1.0;
+  int step = 1;
 
-	float alpha = 10.0;
-	float gamma = 1.0;
-	int step = 1;
+  for (int i = 0; i < step; ++i) {
+    oni.initCompute();
+    std::cout << "initCompute() finished!" << std::endl;
+    oni.compute(alpha, gamma);
+    std::cout << "compute() finished!" << std::endl;
 
-	for (int i = 0; i < step; ++i)
-	{
-		oni.initCompute();
-		std::cout << "initCompute() finished!" << std::endl;
-		oni.compute(alpha, gamma);
-		std::cout << "compute() finished!" << std::endl;
+    char num_str[100];
+    sprintf(num_str, "%d", i);
+    plyWriter->SetFileName(
+        (template_filename.substr(0, template_filename.find_last_of('.')) +
+         "_" + num_str)
+            .c_str());
+    plyWriter->Write();
+    alpha = alpha * 0.5;
+  }
+  vtkSmartPointer<vtkPolyDataMapper> template_mapper =
+      vtkSmartPointer<vtkPolyDataMapper>::New();
+  template_mapper->SetInputData(template_polyData);
 
-		char num_str[100];
-		sprintf(num_str, "%d", i);
-		plyWriter->SetFileName( (template_filename.substr(0, template_filename.find_last_of('.')) + "_" + num_str).c_str() );
-		plyWriter->Write();
-        alpha = alpha * 0.5;
-	}
-	vtkSmartPointer<vtkPolyDataMapper> template_mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
- 	template_mapper->SetInputData(template_polyData);
+  vtkSmartPointer<vtkPolyDataMapper> target_mapper =
+      vtkSmartPointer<vtkPolyDataMapper>::New();
+  target_mapper->SetInputData(target_polyData);
 
-  	vtkSmartPointer<vtkPolyDataMapper> target_mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-  	target_mapper->SetInputData(target_polyData);
+  vtkSmartPointer<vtkActor> template_actor = vtkSmartPointer<vtkActor>::New();
+  template_actor->SetMapper(template_mapper);
+  template_actor->GetProperty()->SetColor(0.8, 0.2, 0);
 
-  	vtkSmartPointer<vtkActor> template_actor = vtkSmartPointer<vtkActor>::New();
-  	template_actor->SetMapper(template_mapper);
-  	template_actor->GetProperty()->SetColor(0.8,0.2,0);
+  vtkSmartPointer<vtkActor> target_actor = vtkSmartPointer<vtkActor>::New();
+  target_actor->SetMapper(target_mapper);
+  target_actor->GetProperty()->SetColor(0, 0.2, 0.8);
 
-  	vtkSmartPointer<vtkActor> target_actor = vtkSmartPointer<vtkActor>::New();
-  	target_actor->SetMapper(target_mapper);
-  	target_actor->GetProperty()->SetColor(0,0.2,0.8);
+  vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+      vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+      vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
 
-  	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
-  	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
-  	renderWindow->AddRenderer(renderer);
-  	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  	renderWindowInteractor->SetRenderWindow(renderWindow);
+  renderer->AddActor(template_actor);
+  renderer->AddActor(target_actor);
+  renderer->SetBackground(0.1804, 0.5451, 0.3412);  // Sea green
 
-  	renderer->AddActor(template_actor);
-  	renderer->AddActor(target_actor);
-  	renderer->SetBackground(0.1804,0.5451,0.3412); // Sea green
+  vtkSmartPointer<vtkInteractorStyleRubberBandPick> interactorStyle =
+      vtkSmartPointer<vtkInteractorStyleRubberBandPick>::New();
+  renderWindowInteractor->SetInteractorStyle(interactorStyle);
 
-	vtkSmartPointer<vtkInteractorStyleRubberBandPick> interactorStyle = vtkSmartPointer<vtkInteractorStyleRubberBandPick>::New();
-	renderWindowInteractor->SetInteractorStyle(interactorStyle);
-
-  	renderWindow->Render();
-  	renderWindowInteractor->Start();
-	return 0;
+  renderWindow->Render();
+  renderWindowInteractor->Start();
+  return 0;
 }
